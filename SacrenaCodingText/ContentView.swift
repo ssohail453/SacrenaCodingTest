39c56d769a059e4919ab9e0cd176159e66ac3167
ssohail453 <syed.sohail@NuLaptop404.lan>
2024-09-14 21:39:56 +0530

    First commit

---
+++ SacrenaCodingText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SacrenaCodingText
+//
+//  Created by syed.sohail on 14/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SacrenaCodingText/SacrenaCodingTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SacrenaCodingTextApp.swift
+//  SacrenaCodingText
+//
+//  Created by syed.sohail on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SacrenaCodingTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
